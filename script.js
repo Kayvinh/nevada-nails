2cce15dd6323fd6a997aae2657dc5e6c786698e3
git config --global user.name kevingit config --global user.name <kayvinhnguyen@gmail.com>
2021-04-16 23:44:44 -0700

    menu page

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function initMap() {
-    let location = {lat: 36.195720, lng: -115.118680};
-    let map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: location
+    const location = {lat: 36.195250, lng: -115.118500};
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 18,
+        center: location,
+    });
+
+    new google.maps.Marker({
+        position: location,
+        map,
+        title: "Nevada Nails & Lashes",
     });
 }
